6f2fb8fb61afe50d75970860f8ca6e9792bb246e
Param Pragyan <parampragyan@gmail.com>
2023-09-28 21:57:05 +0530

    Updated tabs of the stats page

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { getUserStats, updateTimeSpent } from "../../helper/helper";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { authCtx } from "../../store/auth-context";
+import { div } from "@tensorflow/tfjs";
 
 const Stats = () => {
   const [stats, setStats] = useState([]);
@@ -59,35 +60,119 @@ const Stats = () => {
     setLoading(true);
     userAndStatHandler();
   }, []);
+
+  //  tabs
+
+  const calculateStatistics = () => {
+    let totalSpentTime = 0;
+    let totalNotified = 0;
+    let last7DaysAvg = "0:00"; 
+    let lastDayTimeSpent = 0;
+
+    if (stats.length > 0) {
+      stats.forEach((stat) => {
+        totalSpentTime += stat.timeSpent;
+        totalNotified += stat.notified;
+      });
+
+      const last7DaysStats = stats.slice(-7);
+      if (last7DaysStats.length > 0) {
+        const totalTimeSpentInLast7Days = last7DaysStats.reduce(
+          (total, stat) => total + stat.timeSpent,
+          0
+        );
+        const avgTimeSpentInMinutes =
+          totalTimeSpentInLast7Days / last7DaysStats.length;
+        const avgHours = Math.floor(avgTimeSpentInMinutes / 60);
+        const avgMinutes = avgTimeSpentInMinutes % 60;
+        last7DaysAvg = `${avgHours}:${avgMinutes < 10 ? "0" : ""}${avgMinutes}`;
+      }
+
+      lastDayTimeSpent = stats[stats.length - 1].timeSpent;
+
+      const totalHours = Math.floor(totalSpentTime / 3600);
+      const totalMinutes = Math.floor((totalSpentTime % 3600) / 60);
+      totalSpentTime = `${totalHours}:${
+        totalMinutes < 10 ? "0" : ""
+      }${totalMinutes}`;
+
+      const lastDayHours = Math.floor(lastDayTimeSpent / 3600);
+      const lastDayMinutes = Math.floor((lastDayTimeSpent % 3600) / 60);
+      lastDayTimeSpent = `${lastDayHours}:${
+        lastDayMinutes < 10 ? "0" : ""
+      }${lastDayMinutes}`;
+    }
+
+    return [
+      { label: "Total Time Spent", value: totalSpentTime },
+      { label: "Number of Notifications", value: totalNotified },
+      { label: "Average Time Spent(Last 7 Days)", value: last7DaysAvg },
+      { label: "Current Day Time Spent", value: lastDayTimeSpent },
+    ];
+  };
+
+  const statistics = calculateStatistics();
+
   return (
     <div className="bg-bgd h-full text-slate-50">
-      <div>{loading && <p>Loading...</p>}</div>
-      <div className="text-3xl">
+      {/* <div className="">{loading && <p></p>}</div> */}
+      {/* <div className="text-1xl mx-48">
         {!loading && <p>{JSON.stringify(stats)}</p>}
-   
-      </div>
+      </div> */}
 
-      {!loading && (
+      {/* {!loading && (
     <div>
       {stats.map((stat, index) => (
         <p key={index}>Time Spent: {stat.timeSpent}</p>
       ))}
     </div>
-  )}
+  )} */}
 
- {!loading && stats.length > 0 ? (
+      {/* {!loading && stats.length > 0 ? (
     <div>
       <p>Last Time Spent: {stats[stats.length - 1].timeSpent}</p>
     </div>
   ) : (
     <p>No stats available</p>
-  )}
-     {/* dashboard navigation */}
+  )} */}
+      {/* dashboard navigation */}
       {/* <div>
         <Link className="text-3xl" to="/dashboard">
           Dashboard
         </Link>
       </div> */}
+
+      {/* start */}
+      <div className="stats-frame px-48 py-28 flex flex-col gap-11 ">
+        <div className="upper-tabs">
+          
+            <div className="flex items-center justify-between gap-11 w-full ">
+              {statistics.map((stat, index) => (
+                <div className="flex" key={index}>
+                  <div className="flex flex-col items-center justify-center gap-4 border border-bgl w-[34.5rem] tabShadow p-10 rounded-xl ">
+                    {!loading ?(
+                      <div className="flex flex-col items-center justify-center gap-4">
+                        <div className="text-8xl font-bold text-bght">{stat.value}</div>
+                               <div className="text-[1.3rem] font-bold text-bgl">{stat.label}</div>
+                      </div>
+                               
+           
+                    ):(
+                      <div className="text-[2.6rem] p-10">
+                        Loding...
+                      </div>
+                    )}
+                    
+                  </div>
+                </div>
+              ))}
+            </div>
+        </div>
+        <div className="charts flex gap-11">
+          <div className="left w-[70%] h-[50rem] border border-bgl rounded-xl tabShadow2 "></div>
+          <div className="right w-[30%] h-[50rem] border border-bgl rounded-xl tabShadow2"></div>
+        </div>
+      </div>
     </div>
   );
 };
