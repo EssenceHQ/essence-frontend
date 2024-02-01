02b1f6fb6f3348e8dadf679cb417ed1698da71db
Param Pragyan <parampragyan@gmail.com>
2024-02-02 00:34:23 +0530

    responsive changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,14 +6,14 @@ import Button from "../UI/Button";
 import { BsGithub } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 import { BsTwitter } from "react-icons/bs";
-import {AiTwotoneThunderbolt} from "react-icons/ai"
+import { AiTwotoneThunderbolt } from "react-icons/ai";
 import leftImg from "../../assets/Icons/right-img.png";
 import logo from "../../../public/logo2.png";
 import classes from "./Home.module.css";
 import { BsCheck2Circle } from "react-icons/bs";
-import bgIcon from "../../assets/Icons/bg-icon.png"
-import bgIcon2 from "../../assets/Icons/medical.png"
-
+import bgIcon from "../../assets/Icons/bg-icon.png";
+import bgIcon2 from "../../assets/Icons/medical.png";
+import CopyRight from "../Footer/CopyRight";
 
 const Home = ({ showAuthHandler }) => {
   const authContext = useContext(authCtx);
@@ -24,10 +24,7 @@ const Home = ({ showAuthHandler }) => {
   };
   return (
     <div className="h-[100vh] relative overflow-hidden flex flex-col justify-around max-md:justify-between w-full bg-bgd home">
-
-
-
-      <div className="bar flex max-md:mt-14 items-center justify-between max-lg:mx-20 mx-48">
+      <div className="bar flex max-md:mt-10 items-center justify-between max-lg:mx-20 max-md:mx-10 mx-48">
         <div className="bar flex  items-center justify-center ">
           <img className="w-10 h-10" src={logo} alt="" />
           <h1 className="logo-txt text-bgl font-[Righteous]  text-b1">
@@ -85,12 +82,15 @@ const Home = ({ showAuthHandler }) => {
             </div> */}
           </div>
 
-
           {/* hidden tag only for small screen */}
           <div className="hidden max-lg:block rounded-xl tabShadow4 mt-10  bg-[#00000050] border border-bgl py-5 px-4">
-            <p className="text-center font-[Montserrat] leading-9 text-xl text-slate-200 ">Essence, a health-focused app for developers, uses Tensorflow to track posture, sets daily PC usage targets, awards points for good posture and goal achievement, and offers ambient sounds for a focused work environment.</p>
+            <p className="text-center font-[Montserrat] leading-9 text-xl text-slate-200 ">
+              Essence, a health-focused app for developers, uses Tensorflow to
+              track posture, sets daily PC usage targets, awards points for good
+              posture and goal achievement, and offers ambient sounds for a
+              focused work environment.
+            </p>
           </div>
-
 
           <div className="nav-btn-container mt-10  ">
             <button className="navbtn ">
@@ -119,23 +119,22 @@ const Home = ({ showAuthHandler }) => {
         </div>
 
         <div className="right max-lg:hidden flex flex-col">
-        
-       <div className="right mr-40 flex flex-col">
-             {/* <div className=" absolute rounded-full top-[-1rem] right-[50rem] rotate-[-45deg] h-[60rem] w-[10rem] bg-transparent border-4 border-[#0e948d8a]"></div> */}
+          <div className="right mr-40 flex flex-col">
+            {/* <div className=" absolute rounded-full top-[-1rem] right-[50rem] rotate-[-45deg] h-[60rem] w-[10rem] bg-transparent border-4 border-[#0e948d8a]"></div> */}
             <div className="img-container absolute border-[#97c0bc]  w-[95rem] h-[90rem]" />
             <div className="left-img w-x1 z-10 h-x1 flex items-center justify-center  bg-bght">
-              <img className="border-4 border-bgl rounded-round r-img " src={leftImg} alt="" />
+              <img
+                className="border-4 border-bgl rounded-round r-img "
+                src={leftImg}
+                alt=""
+              />
             </div>
           </div>
 
           {/* nav */}
-
-          
-
-          
         </div>
       </div>
-      <div className="icon-container flex items-center justify-between mx-52">
+      <div className="icon-container flex max-lg:flex-col items-center justify-between mx-52 max-lg:mb-10 max-lg:mx-0">
         {/* <ul className="icons flex items-center justify-center gap-8">
           <li>
             {" "}
@@ -156,10 +155,14 @@ const Home = ({ showAuthHandler }) => {
             </a>{" "}
           </li>
         </ul> */}
-        <ul className="icons flex gap-10 ml-4 max-lg:w-full items-center justify-center max-md:mb-14">
+
+        <ul className="icons flex gap-10 ml-4 max-lg:ml-0 max-lg:w-full items-center justify-center max-md:mb-10">
           <li>
             {" "}
-            <a href="https://www.linkedin.com/company/essencehq" target="_blank">
+            <a
+              href="https://www.linkedin.com/company/essencehq"
+              target="_blank"
+            >
               <FaLinkedinIn className=" icon w-11 h-11 text-bgl" />
             </a>{" "}
           </li>
@@ -176,9 +179,13 @@ const Home = ({ showAuthHandler }) => {
             </a>{" "}
           </li>
         </ul>
-        <div className="max-lg:hidden" >
+        <div className="z-40 max-lg:bg-transparent bg-bgl py-3 rounded-full px-6 max-lg:p-0">
+          <CopyRight />
+        </div>
+        <div className="max-lg:hidden">
           <h1 className="text-3xl flex items-center font-[Noto+Sans] font-bold px-4 tag">
-            Made with<AiTwotoneThunderbolt className="w-10 h-12 pl-1 text-bgl"/>
+            Made with
+            <AiTwotoneThunderbolt className="w-10 h-12 pl-1 text-bgl" />
           </h1>
         </div>
       </div>
